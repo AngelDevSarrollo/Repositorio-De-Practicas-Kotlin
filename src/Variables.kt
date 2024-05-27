8b947e876028762b0aff423e59380424b1d1ed3d
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-05-27 14:21:05 +0200

    variables

---
+++ src/Variables.kt
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    /*
+    String, Int, Double, Float, Boolean
+     */
+    var v1: Int = 1
+    println(v1)
+    var v2 = 2.1
+    println(v2)
+    v2 = 5.5
+    println(v2)
+    var texto1 = "Hola "
+    var texto2 = "Mundo"
+    println(texto1+texto2)
+
+}
